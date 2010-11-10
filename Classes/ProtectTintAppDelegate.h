a389823811cd03304eaf15416b3320dfb68f62dc
Carl Brown <carlb@pobox.com>
2010-11-09 20:35:00 -0600

    coco2sd initial

---
+++ Classes/ProtectTintAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ProtectTintAppDelegate.h
+//  ProtectTint
+//
+//  Created by Carl Brown on 11/9/10.
+//  Copyright PDAgent, LLC 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProtectTintAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProtectTint
+//
+//  Created by Carl Brown on 11/9/10.
+//  Copyright PDAgent, LLC 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"ProtectTintAppDelegate");
+	[pool release];
+	return retVal;
+}
